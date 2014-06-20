9aacc4bf7769dfaa67d5b444ea6ea20c7e95656d
lusu <lusu@b5m.com>
2014-06-20 16:10:04 +0800

    测试发布一个composer包，helloworld

---
+++ composer/a.php
@@ -0,0 +1,12 @@
+<?php
+require 'vendor/autoload.php';
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+// create a log channel
+$log = new Logger('name');
+$log->pushHandler(new StreamHandler('a.log', Logger::WARNING));
+
+// add records to the log
+$log->addWarning('Foo');
+$log->addError('Bar');
